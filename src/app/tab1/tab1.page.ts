58d6fff7f24bd2200578e3c55bd7ee115d3bc404
antopio26 <antoniopio.maggio@live.it>
2024-01-10 16:24:05 +0100

    Updated tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem, IonButton, IonRange, IonDatetimeButton, IonDatetime, IonModal } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem, IonButton, IonRange, IonDatetimeButton, IonDatetime, IonModal],
 })
 export class Tab1Page {
+
   constructor() {}
 }
