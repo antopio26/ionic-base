b673e0b2e10ce13ffdbff6d9e582b0554586c218
antopio26 <antoniopio.maggio@live.it>
2024-01-12 14:49:38 +0100

    modificato

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem, IonButton, IonRange, IonDatetimeButton, IonDatetime, IonModal } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton, IonRange, IonDatetimeButton, IonDatetime, IonModal, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem, IonButton, IonRange, IonDatetimeButton, IonDatetime, IonModal],
+  imports: [FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton, IonRange, IonDatetimeButton, IonDatetime, IonModal, IonInput],
 })
 export class Tab1Page {
 
   public age: number = 0;
+  public maxAge: number = 100;
 
   constructor() {}
 }
